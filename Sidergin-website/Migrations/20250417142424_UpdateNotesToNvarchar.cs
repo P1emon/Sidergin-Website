14bf38ffb5ea68d58aff87e4cd3ca9701d96a10e
Duc Truong <truongduc500@gmail.com>
2025-04-17 21:48:21 +0700

    sửa lỗi font lịch xử giao dịch, thêm thông báo xử lí khi đơn hàng sỉ chờ lâu

---
+++ Sidergin-website/Migrations/20250417142424_UpdateNotesToNvarchar.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sidergin_website.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNotesToNvarchar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "notes",
+                table: "orders",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldUnicode: false,
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "notes",
+                table: "orders",
+                type: "varchar(500)",
+                unicode: false,
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
